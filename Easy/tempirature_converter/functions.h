1f6eb19f66d299339f62f6a3d7f512d3e7c0073a
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-01-22 19:55:48 +0100

    I fixed some problems on the logic

--- Easy/tempirature_converter/functions.h
+++ Easy/tempirature_converter/functions.h
@@ -5,7 +5,7 @@
 
 
 // Every function has two versions, the first one always uses the formula and returns the result that we want.
-// The second one checks the edge cases of our value because there is no value under 273.15k theoretically.
+// The second one checks the edge cases of our value because there is no value under 0k theoretically.
 // I use this method to re-use our function inside other functions without printing anything.
 
 double cel_to_fahr(double cel) {
@@ -17,11 +17,11 @@ double fahr_to_cel(double fahr) {
 }
 
 double cel_to_kelv(double cel) {
-  return (cel - 273.15); // celsuis to kelvin
+  return (cel + 273.15); // celsuis to kelvin
 }
 
 double kelv_to_cel(double kelv) {
-  return (kelv + 273.15); // kelvin to celsuis
+  return (kelv - 273.15); // kelvin to celsuis
 }
 
 double kelv_to_fahr(double kelv) {
@@ -37,56 +37,56 @@ double fahr_to_kelv(double fahr) {
 void  celsuis_to_fahrenheit(double value) {
   double fahrenheit {cel_to_fahr(value)};
 
-  if (fahr_to_kelv(fahrenheit) >= -273.15)
+  if (fahr_to_kelv(fahrenheit) >= 0)
     std::cout << "Tempurature in fahrenheit: " << fahrenheit << "F" << std::endl;
   else
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
 }
 
 void fahrenheit_to_celsuis(double value) {
   double celsuis {fahr_to_cel(value)};
 
-  if (cel_to_kelv(celsuis) >= -273.15)
+  if (cel_to_kelv(celsuis) >= 0)
     std::cout << "Tempurature in celsuis: " << celsuis << "C" << std::endl;
   else
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
 }
 
 void celsuis_to_kelvin(double value) {
   double kelvin {cel_to_kelv(value)};
 
-  if (kelvin >= -273.15)
+  if (kelvin >= 0)
     std::cout << "Tempurature in kelvin: " << kelvin << "K" << std::endl;
   else
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
 }
 
 void kelvin_to_celsuis(double value) {
   double celsuis {kelv_to_cel(value)};
 
-  if (value >= -273.15) {
+  if (value >= 0) {
     std::cout << "Tempurature in celsuis: " << celsuis << "C" << std::endl;
   } else {
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
   }
 }
 
 void kelvin_to_fahrenheit(double value) {
   double fahrenheit {kelv_to_fahr(value)};
-  if (value >= -273.15) {
+  if (value >= 0) {
     std::cout << "Tempurature in fahrenheit: " << fahrenheit << "F" << std::endl;
   } else {
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
   }
 }
 
 void fahrenheit_to_kelvin(double value) {
   double kelvin {fahr_to_kelv(value)};
 
-  if (kelvin >= 273.15)
+  if (kelvin >= 0)
     std::cout << "Temperature in kelvin: " << kelvin << "K" << std::endl;
   else
-    std::cout << "Impossible!, there's no termperature under -273.15k" << std::endl;
+    std::cout << "Impossible!, there's no termperature under 0k" << std::endl;
 }
 
 #endif  // FUNCTIONS_H
