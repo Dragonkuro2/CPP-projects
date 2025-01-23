c36e72f1859637f3576ad994afa2e92a67ca8330
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-01-23 13:13:45 +0100

    header files with all prototypes used on the project

---
+++ Easy/Grade_calculator/grades.h
@@ -0,0 +1,14 @@
+#ifndef GRAD_H
+#define GRAD_H
+
+#include <iostream>
+
+
+std::string mark_checker(double result);
+double *Get_marks();
+void GPA_calculator();
+void Percent_calculator();
+void Grad_calculator();
+
+
+#endif // GRAD_H
