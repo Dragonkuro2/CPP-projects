0dcddb32ca111b38d4c3d7eb6f0fb792503fb175
Hicham Bourezi <Hicham.bourezi@gmail.com>
2025-01-23 11:37:27 +0100

    main function that has the menu

---
+++ Easy/Grade_calculator/main.cpp
@@ -0,0 +1,26 @@
+#include "grad.h"
+
+int main() {
+  int option {};
+  do {
+    std::cout << "\n\t************************ MENU ************************\n\n";
+    std::cout << "1) Calculat your grad." << std::endl;
+    std::cout << "2) Close." << std::endl;
+    std::cout << "\n\tEnter your choice: ";
+    std::cin >> option;
+
+    switch (option) {
+      case 1:
+        Grad_calculator();
+        break;
+      case 2:
+        std::cout << "GoodBye!" << std::endl;
+        break;
+      default:
+        std::cout << "Enter a valid option!" << std::endl;
+        break;
+    }
+  } while (option != 2);
+
+  return 0;
+}
